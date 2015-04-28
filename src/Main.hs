a43cde3fb61bdd21a5335dfb17b5309fcbe7ca3f
Jeffrey Pratt <@clementi1832>
2015-04-28 12:51:31 -0600

    Add types to functions

--- src/Main.hs
+++ src/Main.hs
@@ -15,15 +15,21 @@ maxLineLength s = maximum . map length $ lines s
 countWords :: String -> Int
 countWords = length . words
 
+usage :: IO ()
 usage = do
   progName <- getProgName
   putStrLn ("Usage: " ++ progName ++ " [OPTION]... [FILE]...")
 
+version :: IO ()
 version = putStrLn "Haskell wc 0.1"
 
+exit :: IO a
 exit = exitWith ExitSuccess
+
+die :: IO a
 die = exitWith (ExitFailure 1)
 
+parse :: [String] -> IO String
 parse [] = getContents
 parse ["-h"] = usage >> exit
 parse ["-v"] = version >> exit
