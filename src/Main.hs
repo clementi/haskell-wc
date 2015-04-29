a60af30454b5ba417fb2745fb48ee892cd585b29
Jeff Pratt <me@jeffreypratt.net>
2015-04-28 21:48:29 -0600

    Extract function 'readFiles'

--- src/Main.hs
+++ src/Main.hs
@@ -29,19 +29,22 @@ exit = exitWith ExitSuccess
 die :: IO a
 die = exitWith (ExitFailure 1)
 
+readFiles :: [String] -> IO String
+readFiles filenames = concat `fmap` mapM readFile filenames
+      
 parse :: [String] -> IO Int
 parse [] = getContents >>= return . countLines
 parse ["-w"] = getContents >>= return . countWords
 parse ["-c"] = getContents >>= return . countChars
 parse ["-l"] = getContents >>= return . countLines
 parse ["-L"] = getContents >>= return . maxLineLength
-parse ("-w":filenames) = concat `fmap` mapM readFile filenames >>= return . countWords
-parse ("-c":filenames) = concat `fmap` mapM readFile filenames >>= return . countChars
-parse ("-l":filenames) = concat `fmap` mapM readFile filenames >>= return . countLines
-parse ("-L":filenames) = concat `fmap` mapM readFile filenames >>= return . maxLineLength
+parse ("-w":filenames) = readFiles filenames >>= return . countWords
+parse ("-c":filenames) = readFiles filenames >>= return . countChars
+parse ("-l":filenames) = readFiles filenames >>= return . countLines
+parse ("-L":filenames) = readFiles filenames >>= return . maxLineLength
 parse ["--help"] = usage >> exit
 parse ["--version"] = version >> exit
-parse filenames = concat `fmap` mapM readFile filenames >>= return . countLines
+parse filenames = readFiles filenames >>= return . countLines
 
 main :: IO ()
 main = getArgs >>= parse >>= print
